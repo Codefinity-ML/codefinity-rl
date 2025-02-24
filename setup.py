ddbc6199fe93a3cf5871fdf9febdc0811a6ec19d
Codefinity-ML <artem.hrechka@codefinity.com>
2025-02-24 18:05:34 +0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codefinity-rl",
+    version="0.1.0",
+    description="RL environmets for course tasks on Codefinity platform",
+    packages=find_packages(),
+    install_requires=["gymnasium"],
+)
