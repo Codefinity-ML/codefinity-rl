4b65315464cf47d603f1d15f8456d39b0e0788af
Codefinity-ML <artem.hrechka@codefinity.com>
2025-02-28 15:02:16 +0200

    Modified __init__.py to include new assistance functions

--- codefinityrl/challenges/mab/__init__.py
+++ codefinityrl/challenges/mab/__init__.py
@@ -8,8 +8,12 @@ __all__ = [
     "hint3",
     "solution3",
     "check3",
+    "hint4",
+    "solution4",
+    "check4",
 ]
 
 from codefinityrl.challenges.mab.task1 import hint1, solution1, check1
 from codefinityrl.challenges.mab.task2 import hint2, solution2, check2
 from codefinityrl.challenges.mab.task3 import hint3, solution3, check3
+from codefinityrl.challenges.mab.task4 import hint4, solution4, check4
