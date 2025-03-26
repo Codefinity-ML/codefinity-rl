bbeba4e30696c10015e6c8946d60e94f47a6bfa1
Codefinity-ML <artem.hrechka@codefinity.com>
2025-03-26 15:03:05 +0200

    Updated __init__.py for dp module

---
+++ codefinityrl/challenges/dp/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "hint1",
+    "solution1",
+    "check1",
+    "hint2",
+    "solution2",
+    "check2",
+    "hint3",
+    "solution3",
+    "check3",
+]
+
+from codefinityrl.challenges.dp.task1 import hint1, solution1, check1
+from codefinityrl.challenges.dp.task2 import hint2, solution2, check2
+from codefinityrl.challenges.dp.task3 import hint3, solution3, check3
