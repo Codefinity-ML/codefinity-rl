e64d1af6f9d4303ed351119f74c2a6f1245955a5
Codefinity-ML <artem.hrechka@codefinity.com>
2025-04-23 03:22:14 +0300

    Added module exports for task assistance

---
+++ codefinityrl/challenges/td/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["solution1", "check1", "solution2", "check2"]
+
+from codefinityrl.challenges.td.task1 import solution1, check1
+from codefinityrl.challenges.td.task2 import solution2, check2
